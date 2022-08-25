cea5bdf38271e5ccef5f7ba1a2e384c5c0871794
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-25 18:50:57 -0300

    outputs

--- main.tf
+++ main.tf
@@ -59,16 +59,22 @@ resource "aws_cloudsearch_domain" "movies" {
     highlight       = false
     analysis_scheme = "_en_default_"
   }
-
 }
 
-# resource "aws_cloudsearch_domain_service_access_policy" "movies" {
-#   domain_name   = aws_cloudsearch_domain.movies.id
-#   access_policy = file("${path.module}/cloudsearch-policy.json")
+resource "aws_cloudsearch_domain_service_access_policy" "movies" {
+  domain_name   = aws_cloudsearch_domain.movies.id
+  access_policy = file("${path.module}/cloudsearch-policy.json")
 
-#   timeouts {
-#     create = "1h"
-#     update = "1h"
-#     delete = "1h"
-#   }
-# }
+  timeouts {
+    update = "1h"
+    delete = "1h"
+  }
+}
+
+output "search_endpoint" {
+  value = aws_cloudsearch_domain.movies.search_service_endpoint
+}
+
+output "document_endpoint" {
+  value = aws_cloudsearch_domain.movies.document_service_endpoint
+}
